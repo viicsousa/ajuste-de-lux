3ad4a0f74579212f15a1a496e77cec754e965f3b
Victória <victoria.vasconcelos@hotmail.com>
2023-06-16 13:42:20 -0300

    feat:

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Logo from '../assets/Logo Rocketseat.svg'
 import Tag from '../assets/Tag.svg'
 export default function Home() {
   return (
-    <main className="grid min-h-screen grid-cols-2 bg-black">
+    <main className="grid min-h-screen grid-cols-2 bg-black border-b-8 border-violet-500">
       <div className="relative flex flex-col items-start justify-between overflow-hidden p-32">
         <Image src={Logo} alt="" />
         <h1 className="text-7xl font-bold">Ajuste de iluminação</h1>
